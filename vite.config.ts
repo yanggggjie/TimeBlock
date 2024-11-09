c1d463711270a660a8339b585062635c0b78e8e6
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-11-09 16:27:06 +0800

    feat: add block

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import generouted from '@generouted/react-router/plugin'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/TimeBlock/',
-  plugins: [react(), generouted()],
+  plugins: [react(), generouted(), svgr()],
 })
