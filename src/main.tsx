d4f19864acb1f0fb9929223a084a0e0727e13697
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-11-09 15:20:48 +0800

    chore: deploy in github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Routes } from '@generouted/react-router'
+import { routes } from '@generouted/react-router'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const Routes = () => (
+  <RouterProvider
+    router={createBrowserRouter(routes, { basename: '/time-block/' })}
+  />
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import generouted from '@generouted/react-router/plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/time-block/',
   plugins: [react(), generouted()],
 })
